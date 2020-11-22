0e8754b0149d94c38485d22eceaac2409aa9ee98
alt277 <altima875@gmail.com>
2020-11-22 18:56:56 +0300

    ok total

--- src/main/java/ru/geekbrains/MainPlain.java
+++ src/main/java/ru/geekbrains/MainPlain.java
@@ -4,8 +4,8 @@ import org.hibernate.cfg.Configuration;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import java.util.List;
-import java.util.Map;
 
 public class MainPlain {
 
@@ -18,21 +18,27 @@ public class MainPlain {
         EntityManager em = emFactory.createEntityManager(); // содаем экземпляр EntityManager
 
         // найти всех покупотелей товара:
-        List<Customer> customers= em.createQuery("    SELECT  c FROM Customer as c" +
+        List<Customer> customers = em.createQuery("    SELECT  c FROM Customer as c" +
                 "        join Ties  as t  on t.customer_id=c.id\n" +
                 "        join Product   p on p.id=t.product_id\n" +
-                "        where p.title='Iphone_11'",Customer.class).getResultList();
+                "        where p.title='Iphone_11'", Customer.class).getResultList();
         System.out.println(customers);
 
         // найти все покупки определенного покупателя
-        List<Product> products= em.createQuery("    SELECT  p FROM Product as p" +
+        List<Product> products = em.createQuery("    SELECT  p FROM Product as p" +
                 "        join Ties  as t  on t.customer_id=p.id\n" +
                 "        join Customer  as c on c.id=t.product_id\n" +
-                "        where c.first_name='Alex'",Product.class).getResultList();
+                "        where c.first_name='Alex'", Product.class).getResultList();
         System.out.println(products);
+        EntityTransaction emTransaction = em.getTransaction();
 
+        emTransaction.begin();
+        Product2 product2 = new Product2();
+        em.merge(product2);
+        em.flush();
+        emTransaction.commit();
 
-       em.close();  //  закрыть менеджер сущностей EntityManager
+        em.close();  //  закрыть менеджер сущностей EntityManager
 
 
     }
